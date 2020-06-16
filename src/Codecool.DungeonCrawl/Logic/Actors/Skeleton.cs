73bf26c59f88f7a3eafce4f32b5895457ed43d65
Mateusz Zawistowski <maturasisko@gmail.com>
2020-06-16 10:50:33 +0200

    Added some random skeleton movement

--- src/Codecool.DungeonCrawl/Logic/Actors/Skeleton.cs
+++ src/Codecool.DungeonCrawl/Logic/Actors/Skeleton.cs
@@ -1,3 +1,5 @@
+using System;
+using Codecool.DungeonCrawl.Logic.Interfaces;
 using Codecool.DungeonCrawl.Logic.Map;
 
 namespace Codecool.DungeonCrawl.Logic.Actors
@@ -5,11 +7,48 @@ namespace Codecool.DungeonCrawl.Logic.Actors
     /// <summary>
     ///     Sample enemy
     /// </summary>
-    public class Skeleton : Actor
+    public class Skeleton : Actor, IUpdatable
     {
+        private static readonly Random _random = new Random();
+        private float _timeLastMove;
+
         public Skeleton(Cell cell) : base(cell, TileSet.GetTile(TileType.Skeleton))
         {
-            // TODO
+            Program.AllUpdatables.Add(this);
+        }
+
+        ~Skeleton()
+        {
+            Program.AllUpdatables.Remove(this);
+        }
+
+        public void Update(float deltaTime)
+        {
+            _timeLastMove += deltaTime;
+
+            if (_timeLastMove <= 0.25f)
+                return;
+
+            _timeLastMove = 0.0f;
+
+            var moveX = _random.Next(-1, 2);
+            var moveY = _random.Next(-1, 2);
+
+            if (moveX == 0 && moveY == 0)
+                return;
+
+            var dir = (moveX, moveY).ToDirection();
+
+            TryMove(dir);
+        }
+
+        private void TryMove(Direction dir)
+        {
+            var targetCell = Cell.GetNeighbour(dir);
+            var canPass = targetCell?.OnCollision(this) ?? false;
+
+            if (canPass)
+                AssignCell(targetCell);
         }
 
         public override bool OnCollision(Actor other)
--- src/Codecool.DungeonCrawl/Logic/Utilities.cs
+++ src/Codecool.DungeonCrawl/Logic/Utilities.cs
@@ -17,6 +17,19 @@ namespace Codecool.DungeonCrawl.Logic
             _ => throw new NotImplementedException(),
         };
 
+        public static Direction ToDirection(this (int x, int y) vector) => vector switch
+        {
+            (0, -1) => Direction.Up,
+            (0, 1) => Direction.Down,
+            (-1, 0) => Direction.Left,
+            (1, 0) => Direction.Right,
+            (-1, -1) => Direction.UpperLeft,
+            (1, -1) => Direction.UpperRight,
+            (-1, 1) => Direction.BottomLeft,
+            (1, 1) => Direction.BottomRight,
+            _ => throw new NotImplementedException(),
+        };
+
         public static bool IsPassable(this TileType tile) => tile switch
         {
             TileType.Empty => true,
